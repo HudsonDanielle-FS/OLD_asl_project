1968041bf104b315f73defb44f0329975e3ea948
HudsonDanielle-FS <110405791+HudsonDanielle-FS@users.noreply.github.com>
2023-05-30 18:46:46 -0500

    functions fixed, connected to nodemon

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
 const express = require('express')
 const app = express()
 
+// GET / HTTP/1.1
+app.get('/', (req, res) => {
+    res.send('Home Page! GET...')
+})
+
+// POST / HTTP/1.1
+app.post('/', (req, res) => {
+    res.send('Home Page! POST...')
+})
+
 // GET /products/2-price-desc
 app.get('/products/all', (req, res) => {
     res.send(`
-        GET Products: ${req.get('page')}, ${req.get('sort')},     ${req.get('order')}
+        GET Products: ${req.get('Page')}, ${req.get('Sort')},     ${req.get('Order')}
     `)
 })
 
 // GET /products/8719-small-red HTTP/1.1
 app.get('/products/:id-:size-:color', (req, res) => {
     res.send(`
-    GET Products: ${req.get('id')}, ${req.get('size')}, ${req.get('color')}
+    GET Products: ${req.params.id}, ${req.params.size}, ${req.params.color}
     `)
 })
 
 // GET /products/8719 HTTP/1.1
 app.get('/products/:id', (req, res) => {
     res.send(`
-    GET Products: ${req.get('id')}
+    GET Products: ${req.params.id}
     `)
 })
 
