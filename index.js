26867a3a42e1dcf0f38eafac41e89a06c76e53d3
HudsonDanielle-FS <110405791+HudsonDanielle-FS@users.noreply.github.com>
2023-05-30 08:22:26 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+
+// GET / HTTP/1.1
+app.get('/', (req, res) => {
+    res.send('Home Page! GET...')
+})
+
+// POST / HTTP/1.1
+app.post('/', (req, res) => {
+    res.send('Home Page! POST...')
+})
+
+// GET /products/683-nike-large-white-shoe HTTP/1.1
+app.get('/products/:productId-:productName', (req, res) => {
+    res.send('Product Page! Product name: ' + req.params.productName +
+    ' Product id: ' + req.params.productId)
+})
+
+// GET /products/nike-large-white-shoe HTTP/1.1
+app.get('/products/:productName', (req, res) => {
+    res.send('Product Page! Product name: ' + req.params.productName)
+})
+
+app.listen(3000)
